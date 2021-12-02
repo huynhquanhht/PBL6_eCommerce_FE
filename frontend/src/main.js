39ecdddd308de5c16cc0782e1c0cdead09b4b38e
huynhquanhht <huynhquanhht@gmail.com>
2021-12-03 01:25:18 +0700

    cođing error-screen

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import '@morioh/v-msg/dist/msg.min.css';
+import msg from '@morioh/v-msg';
+import dotenv from 'dotenv';
 
+dotenv.config();
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
+  msg,
   render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/views/Error/Error401.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <error
+      :errorCode="errorCode"
+      :errorTitle="errorTitle"
+      :errorDetail="errorDetail"
+    />
+    <v-btn class="btn-go-back" @click="goLogin">Go to login</v-btn>
+  </div>
+
+</template>
+
+<script>
+import Error from './Error.vue';
+export default {
+  name: 'Error401',
+  components: {
+    Error,
+  },
+  data: function() {
+    return {
+      errorCode: '401',
+      errorTitle: '401 - Unauthorized',
+      errorDetail:
+        'Sorry, your request could not be processed',
+    };
+  },
+  methods: {
+    goLogin() {
+      this.$router.push({name: 'login'});
+    }
+  }
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.btn-go-back {
+  margin-top: 20px;
+  background-color: #fea200 !important;
+  height: 50px !important;
+  width: 180px;
+  color: #ffffff !important;
+  font: 500 16px Roboto !important;
+  letter-spacing: 0;
+}
+</style>
