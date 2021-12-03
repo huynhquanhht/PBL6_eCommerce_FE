0af1fb06b7a41ce13a462962b84191e6203d08f6
huynhquanhht <huynhquanhht@gmail.com>
2021-12-03 19:06:41 +0700

    coding shop-info-screen

---
+++ frontend/src/views/ShopInfo/ShopInfoForm.vue
@@ -0,0 +1,296 @@
+<template>
+  <div class="shop-info-form">
+    <form id="shop-info-form" class="info-form-block">
+      <div class="info-text">
+        <ValidationProvider name="Họ tên" rules="required" v-slot="{ errors }">
+          <div class="label-input">
+            <div class="label-block">
+              <label class="label">Tên cửa hàng</label>
+            </div>
+            <div class="text-block">
+              <input type="text" class="input" v-model="fullname" />
+              <span class="error-text">{{ errors[0] }}</span>
+            </div>
+          </div>
+        </ValidationProvider>
+        <ValidationProvider name="Địa chỉ" rules="required" v-slot="{ errors }">
+          <div class="label-input">
+            <div class="label-block">
+              <label class="label">Địa chỉ</label>
+            </div>
+            <div class="text-block">
+              <input type="text" class="input" v-model="address" />
+              <span class="error-text">{{ errors[0] }}</span>
+            </div>
+          </div>
+        </ValidationProvider>
+        <ValidationProvider
+          name="Số điện thoại"
+          rules="required|phone"
+          v-slot="{ errors }"
+        >
+          <div class="label-input">
+            <div class="label-block">
+              <label class="label">Số điện thoại</label>
+            </div>
+            <div class="text-block">
+              <input type="text" class="input" v-model="phone" />
+              <span class="error-text">{{ errors[0] }}</span>
+            </div>
+          </div>
+        </ValidationProvider>
+        <ValidationProvider name="Mô tả" rules="required" v-slot="{ errors }">
+          <div class="label-input">
+            <div class="label-block">
+              <label class="label">Mô tả</label>
+            </div>
+            <div class="text-block">
+              <textarea class="textarea input" v-model="description"></textarea>
+              <span class="error-text">{{ errors[0] }}</span>
+            </div>
+          </div>
+        </ValidationProvider>
+        <v-dialog v-model="dialog" width="400px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="btn-regis-shop white--text"
+              height="36px"
+              width="120px"
+              color="#fea200"
+              depressed
+              v-bind="attrs"
+              v-on="on"
+              @click="registerShop"
+            >
+              <span class="font-size: 15px">Đăng ký</span>
+            </v-btn>
+
+            <v-btn
+              class="btn-regis-shop white--text"
+              height="36px"
+              width="120px"
+              color="#fea200"
+              depressed
+              v-bind="attrs"
+              v-on="on"
+              @click="registerShop"
+            >
+              <span class="font-size: 15px">Cập nhật</span>
+            </v-btn>
+
+                        <v-btn
+              class="btn-regis-shop white--text"
+              height="36px"
+              width="120px"
+              color="#fea200"
+              depressed
+              v-bind="attrs"
+              v-on="on"
+              @click="registerShop"
+            >
+              <span class="font-size: 15px">Kích hoạt</span>
+            </v-btn>
+          </template>
+          <confirm-dialog
+            :question="question"
+            @agree-confirm-dialog="agree"
+            @cancel-confirm-dialog="cancel"
+          ></confirm-dialog>
+        </v-dialog>
+      </div>
+      <div class="register-ava">
+        <img class="shop-avatar" :src="shopAvatarData" alt="shop-avatar" />
+        <input
+          class="file-input"
+          id="file"
+          type="file"
+          accept="image/gif,image/jpg,image/png,image/svg,image/jpeg"
+          @change="chooseImage($event)"
+        />
+        <label class="choose-image" for="file">
+          Chọn ảnh nền
+        </label>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { required } from 'vee-validate/dist/rules';
+import { extend, ValidationProvider, setInteractionMode } from 'vee-validate';
+import ConfirmDialog from '@/components/ConfirmDialog.vue';
+import { mapGetters, mapActions } from 'vuex';
+setInteractionMode('eager');
+extend('required', {
+  ...required,
+  message: '{_field_} không thể trống',
+});
+extend('phone', phone => {
+  if (!phone.match(/^[0-9]{10,10}$/)) {
+    return 'Số điện thoại phải đủ 10 chữ số';
+  }
+  return '';
+});
+export default {
+  name: 'ShopInfoForm',
+  props: {
+    shopInfo: { type: Object },
+  },
+
+  components: {
+    ValidationProvider,
+    ConfirmDialog,
+  },
+  data() {
+    return {
+      fullname: '',
+      address: '',
+      phone: '',
+      description: '',
+      dialog: false,
+      question: 'Bạn chắc chắn muốn đăng ký cửa hàng?',
+      shopAvatarData: false,
+      shopAvatar: null,
+    };
+  },
+  computed: {
+    ...mapGetters({}),
+  },
+  methods: {
+    ...mapActions({
+      fetchRegisterShop: 'REGISTER_SHOP',
+    }),
+    async agree() {
+      let shopInfo = new FormData();
+      shopInfo.append('Name', this.fullname);
+      shopInfo.append('Address', this.address);
+      shopInfo.append('PhoneNumber', this.phone);
+      shopInfo.append('Description', this.shopDescription);
+      shopInfo.append('ImageFile', this.shopAvatar);
+      await this.fetchRegisterShop(shopInfo);
+      this.dialog = false;
+    },
+    cancel() {
+      this.dialog = false;
+    },
+    chooseImage(e) {
+      let reader = new FileReader();
+      this.shopAvatar = e.target.files[0];
+      reader.onload = e => {
+        this.shopAvatarData = e.target.result;
+      };
+      reader.readAsDataURL(this.shopAvatar);
+    },
+    registerShop() {
+      this.dialog = true;
+    },
+  },
+  created() {
+    this.fullname = this.shopInfo.nameOfShop;
+    this.address = this.shopInfo.address;
+    this.phone = this.shopInfo.phoneNumber;
+    this.description = this.shopInfo.description;
+    this.shopAvatar = process.env.VUE_APP_BASE_URL + this.shopInfo.avatar;
+  },
+};
+</script>
+
+<style scoped>
+.info-form-block {
+  display: flex;
+}
+.label-input {
+  display: grid;
+  grid-template-columns: 120px 280px;
+  column-gap: 10px;
+}
+.label-block {
+  height: 32px;
+  justify-self: end;
+}
+.label {
+  font: 400 15px Roboto;
+  line-height: 32px;
+}
+.input {
+  border: solid 1px #616161;
+  background-color: #ffffff;
+  height: 32px;
+  width: 260px;
+  outline: #616161;
+  padding: 8px;
+  box-shadow: 0px 2px 2px 0px rgb(0 0 0 / 14%);
+  font: 400 15px Roboto;
+  border-radius: 4px;
+}
+
+.input:focus {
+  box-shadow: 0px 0px 8px #616161;
+}
+
+.textarea {
+  height: 100px;
+}
+
+.error-text {
+  display: block;
+  color: red;
+  font: 400 13px Roboto;
+  align-self: start;
+  margin-left: 10px;
+  height: 13px;
+  margin-top: 3px;
+  margin-bottom: 3px;
+}
+
+.btn-regis-shop {
+  margin-top: 5px;
+  text-transform: none;
+  letter-spacing: 0;
+  font: 400 15px Roboto !important;
+  margin-left: 200px;
+}
+
+.shop-avatar {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  box-shadow: 0px 0px 4px 1px #616161;
+}
+
+.register-ava {
+  display: flex;
+  flex-direction: column;
+  margin-left: 80px;
+}
+
+.custom-file-input {
+  margin-top: 18px;
+}
+
+input[type='file'] {
+  display: none;
+}
+
+.file-input {
+}
+
+.choose-image {
+  text-align: center;
+  cursor: pointer;
+  background-color: #fea200;
+  color: #ffffff;
+  font: 400 14px Roboto;
+  line-height: 24px;
+  padding: 5px;
+  border-radius: 4px;
+  margin-top: 10px;
+}
+
+.choose-image:hover {
+  background-color: #f8a71a;
+}
+.choose-image:focus {
+  background-color: #f7a20f;
+}
+</style>
