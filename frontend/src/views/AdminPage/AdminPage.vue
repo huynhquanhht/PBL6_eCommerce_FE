6476e242d2501ca9754958a9a4b7e34298651854
TranHuyTung <tranhuytung1310@gmail.com>
2021-12-16 20:16:07 +0700

    upload shop-management screen for admin

---
+++ frontend/src/views/AdminPage/AdminPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="admin">
+    <div class="sidebar-admin-block">
+      <sidebar-admin class="sidebar-admin"></sidebar-admin>
+    </div>
+    <div class="admin-content"> 
+      <div class="admin-sub">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+import SidebarAdmin from '@/components/SidebarAdmin.vue';
+
+export default {
+  components: { 
+    SidebarAdmin,
+  },
+};
+</script>
+
+<style scoped>
+
+.sidebar-admin-block {
+  position: relative;
+}
+.admin {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 220px 1fr;
+  background-color: #f5f5f5;
+}
+
+.admin-content {
+  margin-top: 5px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+.admin-sub {
+  background-color: #ffffff;
+  width: 940px;
+}
+</style>
---
+++ frontend/src/views/AdminPage/ShopManagement/OrderManagement.vue
@@ -0,0 +1,317 @@
+<template>
+  <div class="shop-management-wrapper">
+    <top-title :title="title"></top-title>
+    <hr class="hr" />
+    <div class="header__top-search-block">
+      <label>Từ ngày: </label>
+      <v-menu
+        ref="menu1"
+        v-model="menu1"
+        :close-on-content-click="false"
+        :return-value.sync="datePicker1"
+        transition="scale-transition"
+        offset-y
+        min-width="270px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="datePicker1"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+            color="#fea200"
+            height="36px"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="datePicker1"
+          scrollable
+          width="270px"
+          year-icon="mdi-calendar-blank"
+          prev-icon="mdi-skip-previous"
+          next-icon="mdi-skip-next"
+          color="#fea200"
+        >
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu1 = false"> Cancel </v-btn>
+          <v-btn text color="primary" @click="$refs.menu1.save(datePicker1)">
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+
+      <label>Đến ngày: </label>
+      <v-menu
+        ref="menu2"
+        v-model="menu2"
+        :close-on-content-click="false"
+        :return-value.sync="datePicker2"
+        transition="scale-transition"
+        offset-y
+        min-width="270px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="datePicker2"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+            color="#fea200"
+            height="36px"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="datePicker2"
+          scrollable
+          width="270px"
+          year-icon="mdi-calendar-blank"
+          prev-icon="mdi-skip-previous"
+          next-icon="mdi-skip-next"
+          color="#fea200"
+        >
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu2 = false"> Cancel </v-btn>
+          <v-btn text color="primary" @click="$refs.menu2.save(datePicker2)">
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+
+      <v-btn @click="search(datePicker1, datePicker2)">Tìm kiếm</v-btn>
+    </div>
+    <div class="shop-table" v-if="allOrders">
+      <table class="styled-table">
+        <thead>
+          <tr>
+            <th>Mã đơn hàng</th>
+            <th>Tên người mua</th>
+            <th>Tên cửa hàng</th>
+            <th>Trạng thái</th>
+            <th>Ngày tạo</th>
+            <th>Tổng hóa đơn</th>
+            <th>Thao tác</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="order in allOrders" :key="order.id">
+            <td>{{ order.id }}</td>
+            <td>{{ order.shipName }}</td>
+            <td>{{ order.shopName }}</td>
+            <td>{{ order.state }}</td>
+            <td>{{ order.orderDate }}</td>
+            <td>{{ order.totalPrice }}</td>
+            <td>
+              <v-btn 
+              icon
+              @click="orderDetailForm(order)">
+                <v-icon size="16px">fa-eye</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <v-dialog v-model="orderDetail">
+        <order-detail
+        :eachOrder="oneOrder"
+        ></order-detail>
+      </v-dialog>
+    </div>
+    <div v-else>
+      <no-content-form
+        :showOrder="true"
+        Notification="Không có dữ liệu hóa đơn"
+      ></no-content-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopTitle from '@/components/TopTitle.vue';
+import OrderDetail from './OrderDetail.vue';
+import NoContentForm from '../../../components/NoContentForm.vue';
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  // name: 'shop-management',
+  components: {
+    TopTitle,
+    OrderDetail,
+    NoContentForm,
+  },
+  data() {
+    return {
+      title: 'Quản lý hóa đơn',
+      orderDetail: false,
+      datePicker1: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      datePicker2: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      menu1: false,
+      menu2: false,
+      oneOrder: null,
+      //orderDetails: null,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      allOrders: 'GET_ALL_ORDERS',
+    }),
+  },
+  methods: {
+    ...mapActions({
+      getAllOrders: 'ACT_GET_ALL_ORDERS',
+    }),
+    orderDetailForm(order) {
+      this.oneOrder = order;
+      //this.OrderDetails = order.OrderDetails;
+      console.log(this.oneOrder);
+      this.orderDetail = true;
+    },
+  },
+  async created() {
+    await this.getAllOrders({fromDate: '01/01/2000', toDate: '12/31/2099'});
+  }
+};
+</script>
+
+<style scoped>
+.v-application p {
+  margin-left: 0px;
+}
+.shop-management-wrapper {
+  background-color: #ffffff;
+  width: 940px;
+  padding: 0px 20px;
+}
+
+.title-block {
+  margin-top: 0px;
+  margin-left: 0px;
+}
+.top-title {
+  height: 44px;
+  color: #616161;
+  border-bottom: 1px solid #616161 !important;
+}
+
+.title-block .title p {
+  margin-left: 0px !important;
+}
+
+.hr {
+  margin-bottom: 5px;
+  border-width: 0;
+  padding-top: 1px;
+  background-color: #616161;
+}
+
+.header__top-search-block {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.header__top-search-block label {
+  width: 120px;
+  font: 400 16px Roboto;
+  color: #616161;
+  padding: auto;
+}
+
+.header__top-search-block .v-text-field {
+  max-width: 270px;
+  margin-left: -20px;
+  margin-right: 40px;
+  font: 400 15px Roboto;
+  color: #616161;
+}
+
+.header__top-search-block input {
+  border: solid 2px #fea200;
+  background-color: #ffffff;
+  height: 36px;
+  width: 520px;
+  margin-left: 0;
+  outline: #fea200;
+  padding: 8px;
+  box-shadow: 0px 2px 2px 0px rgb(0 0 0 / 14%);
+  font: 400 15px Roboto;
+  border-radius: 4px;
+}
+
+.header__top-search-block input:focus {
+  box-shadow: 0px 0px 8px #fea200;
+}
+
+.header__top-search-block .v-btn {
+  height: 36px;
+  color: #ffffff !important;
+  margin-left: 8px;
+  background-color: #fea200 !important;
+  text-transform: none;
+  font: 400 14px Roboto;
+  letter-spacing: 0;
+  box-shadow: none !important;
+}
+/* .shop-table {
+  display: flex;
+  flex-direction: column;
+  column-gap: 20px;
+  height: 32px;
+  width: 100%;
+}
+
+.shop-table table th {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font: 500 14px Roboto;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+} */
+
+.styled-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font: 400 15px Roboto;
+  width: 900px;
+}
+
+.styled-table thead tr {
+  background-color: #fea200;
+  color: #ffffff;
+  text-align: left;
+}
+
+.styled-table th {
+  padding: 12px 15px;
+}
+
+.styled-table td {
+  padding: 3px 15px;
+}
+.product-name-img {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+  font: 400 14px Roboto;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #fea200;
+}
+
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #fea200;
+}
+</style>
